4190015ed746ec60b1093065bf913306e5315d53
nicoCorbal <nicocorbal@gmail.com>
2025-07-14 23:21:34 +0200

    Ups

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://OSIX-Tech.github.io',
-  base: '/Landingv2/',
   integrations: [tailwind(), 
     react()
   ],
