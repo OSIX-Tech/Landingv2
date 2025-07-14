8401110dce3bd68f5fa35f9e506f8576dc366eff
nicoCorbal <nicocorbal@gmail.com>
2025-07-14 23:17:37 +0200

    ci: Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://OSIX-Tech.github.io',
+  base: '/Landingv2/',
   integrations: [tailwind(), 
     react()
   ],
